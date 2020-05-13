e44036ef63041c33ff1060e460aca49c2e6ece25
emas-paul <pjobiggi@gmail.com>
2020-05-13 20:22:26 +0100

    Start of project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <home-2></home-2>
+  </div>
+</template>
+
+<script>
+
+// Imported files
+import HomeBusinessVue from './components/Home-Business.vue';
+
+export default {
+    components: {
+      "home-2": HomeBusinessVue 
+    }
+}
+</script>
